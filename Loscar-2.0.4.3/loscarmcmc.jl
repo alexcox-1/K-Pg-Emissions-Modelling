a0b50ff0b1c564e3a8fc134f935c73d8b03f6ad9
= <=>
2020-11-13 12:39:03 -0500

    Updated sulfate emissions files

--- Loscar-2.0.4.3/loscarmcmc.jl
+++ Loscar-2.0.4.3/loscarmcmc.jl
@@ -12,8 +12,31 @@ time .= (time .- minimum(time)) .* 1000000;
 # the kpg boundary is at 500 kyr.
 
 timevals1 = 1:5000:500001;
+# we have to include a special one year bin for the 
+# so-called impact event
 timevals2 = 500002:5000:1500002;
 
 timevals = [timevals1;timevals2];
+
+# we now have a length 302 time array. let's fill it with
+# co2 and so2 emissions.
+# characteristic Pg/y will be 0.01 - 0.1
+co2vals = zeros(302) .+ 0.01;
+svals = zeros(302) .+ 0.01;
+
+co2doublingrate = 3;
+
+# do a loscar run!
+
+tmv,pco2,loscartemp = runloscar(timevals,co2vals,svals,co2doublingrate);
+
+# apply the sulfate correction
+# convert svals to pinatubos a year
+pinatubos = svals ./ 0.009;
+sulfatecorr = -11.3 .* (1 .- exp.(-0.0466.*pinatubos));
+
+loscartempwsulf = loscartemp .- sulfatecorr;
+
+
 
 
