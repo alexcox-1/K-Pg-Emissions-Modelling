c30071efbadaa01d1564d5d4a35cded565f8bb72
= <=>
2020-11-14 17:27:46 -0500

    Update loscarmcmcp

--- LoscarParallel/loscarmcmcp.jl
+++ LoscarParallel/loscarmcmcp.jl
@@ -4,7 +4,7 @@ MPI.Init()
 
 include("runloscarp.jl")
 include("fillnans.jl")
-cd("LoscarParallel")
+
 
 # Get MPI properties
 comm = MPI.COMM_WORLD
@@ -12,8 +12,12 @@ ntasks = MPI.Comm_size(comm)
 rank = MPI.Comm_rank(comm)
 # Test stdout
 print("Hello from $rank of $ntasks processors!\n")
-
-
+# make scratch folder for each task
+prefix = "/scratch/loscar$rank"
+loscdir = "/dartfs-hpc/scratch/alex/K-Pg-Emissions-Modelling/LoscarParallel"
+system("mkdir -p $prefix")
+system("cp -r $loscdir $prefix")
+cd(prefix)
 bsrtemps = importdataset("tempdatabsr.csv",',');
 timev = bsrtemps["time"];
 temp = bsrtemps["temp"];
@@ -74,15 +78,36 @@ lldist = Array{Float64,1}(undef,numiter);
 co2dist = Array{Float64,2}(undef,length(logco2vals),numiter);
 sdist = Array{Float64,2}(undef,length(logsvals),numiter);
 tempwsulfarray = Array{Float64,2}(undef,length(mu),numiter);
-
+# create a record of what other MPI tasks have right now
+all_log_co2 = Array{Float64}(undef, length(logco2vals), ntasks);
+all_log_s = Array{Float64}(undef, length(logsvals), ntasks);
+all_lls = Array{Float64}(undef,ntasks);
+# do the mcmc
 @inbounds for i = 1:numiter
+    # update current prediction
+    copyto!(logco2valsᵣ,logco2vals);
+    copyto!(logsvalsᵣ,logsvals);
+    # Exchange proposals, sometimes
+    if i % num_per_exchange == 0
+        # Exchange current proposals across all MPI tasks
+        MPI.Allgather!(logco2vals, all_log_co2, comm)
+        MPI.Allgather!(logsvals, all_log_s, comm)
+        MPI.Allgather!(lldist[i:i], all_lls, comm)
+        # Choose which proposal we want to adopt
+        map!(x -> x*rand(), all_lls, all_lls)
+        chosen = argmax(all_lls)
+        logco2valsᵣ .= view(all_log_co2, :, chosen)
+        logsvalsᵣ .= view(all_log_s, :, chosen)
+    end
+    # choose which indices to perturb
     randsindex = rand(1:300,30);
     randcindex = rand(1:300,30);
-    logco2valsᵣ[randcindex] .= logco2vals[randcindex] + (randn.(30)./20);
-    logsvalsᵣ[randsindex] .= logco2vals[randsindex] + (randn.(30)./20);
-
+    # perturb these indices
+    logco2valsᵣ[randcindex] .+= (randn.(30)./20);
+    logsvalsᵣ[randsindex] .+= (randn.(30)./20);
+    # run loscar with the new values
     tmv,pco2,loscartemp = runloscarp(timev,exp.(logco2valsᵣ),exp.(logsvalsᵣ),co2doublingrate);
-
+    # do the sulfate corrections
     pinatubos = exp.(logsvalsᵣ) ./ 0.009;
     sulfatecorr = -11.3 .* (1 .- exp.(-0.0466.*pinatubos));
     loscartimebin = Int.(floor.(tmv ./ 5000) .+ 1);
@@ -106,6 +131,7 @@ tempwsulfarray = Array{Float64,2}(undef,length(mu),numiter);
         logco2vals .= logco2valsᵣ  
         logsvals .= logsvalsᵣ  
     end
+    # update the latest values
     lldist[i] = ll;
     co2dist[:,i] = logco2vals;
     sdist[:,i] = logsvals;
@@ -113,3 +139,16 @@ tempwsulfarray = Array{Float64,2}(undef,length(mu),numiter);
 
 end
 
+all_ll_dist = MPI.Gather(lldist, 0, comm)
+all_co2_dist = MPI.Gather(co2dist, 0, comm)
+all_s_dist = MPI.Gather(sdist, 0, comm)
+all_temps = MPI.Gather(tempwsulfarray, 0, comm)
+
+if rank == 0
+    writedlm(all_ll_dist,"$loscdir/all_ll_dist.csv",',')
+    writedlm(all_co2_dist,"$loscdir/all_co2_dist.csv",',')
+    writedlm(all_s_dist,"$loscdir/all_s_dist.csv",',')
+    writedlm(all_temps,"$loscdir/all_temps.csv",',')
+end  
+    
+MPI.Finalize()
