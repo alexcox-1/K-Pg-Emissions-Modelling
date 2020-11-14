fb95c8cef1252ee58e2bf029cb6daab39b830b5e
Alexander A. Cox <f004qxp@discovery7.hpcc.dartmouth.edu>
2020-11-14 16:48:21 -0500

    Add assorted cluster scripts

---
+++ LoscarParallel/runloscarmcmc.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Environment variables to tell Julia about our nonstandard install location
+export JULIA_BINDIR="/dartfs-hpc/scratch/alex/julia-1.5.3/bin"
+export JULIA_DEPOT_PATH="/dartfs-hpc/scratch/alex/.julia"
+export JULIA_PROJECT="/dartfs-hpc/scratch/alex/.julia/environments/v1.5/Project.toml"
+export JULIA_LOAD_PATH="/dartfs-hpc/scratch/alex/.julia/environments/v1.5/Project.toml:/dartfs-hpc/scratch/alex/julia-1.5.3/share/julia/stdlib/v1.5"
+# Run Julia!
+$JULIA_BINDIR/julia --project=$JULIA_PROJECT ./loscarmcmcp.jl
