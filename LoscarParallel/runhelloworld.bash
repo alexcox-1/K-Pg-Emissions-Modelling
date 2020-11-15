587f7dcb75cb1b4e76b15ccfec3a97cf3f8d76ac
Alexander A. Cox <f004qxp@discovery7.hpcc.dartmouth.edu>
2020-11-14 19:14:46 -0500

    update

--- LoscarParallel/runhelloworld.bash
+++ LoscarParallel/runhelloworld.bash
@@ -5,4 +5,4 @@ export JULIA_DEPOT_PATH="/dartfs-hpc/scratch/alex/.julia"
 export JULIA_PROJECT="/dartfs-hpc/scratch/alex/.julia/environments/v1.5/Project.toml"
 export JULIA_LOAD_PATH="/dartfs-hpc/scratch/alex/.julia/environments/v1.5/Project.toml:/dartfs-hpc/scratch/alex/julia-1.5.3/share/julia/stdlib/v1.5"
 # Run Julia!
-$JULIA_BINDIR/julia --project=$JULIA_PROJECT ./loscarmcmcp.jl
+$JULIA_BINDIR/julia --project=$JULIA_PROJECT ./helloworld.jl
