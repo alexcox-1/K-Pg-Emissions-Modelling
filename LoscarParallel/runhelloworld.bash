ada02a740b060a5dc2f49b7cab1706836f764ace
Brenhin Keller <cbkeller@discovery7.hpcc.dartmouth.edu>
2020-11-15 23:40:39 -0500

    Fix new JULIA_DIR env variable in bash script

--- LoscarParallel/runhelloworld.bash
+++ LoscarParallel/runhelloworld.bash
@@ -2,7 +2,7 @@
 
 # Environment variables to tell Julia about our nonstandard install location
 # Change these if you change users:
-export JULIA_DIR = "dartfs-hpc/scratch/alex/julia-1.5.3"
+export JULIA_DIR="/dartfs-hpc/scratch/alex/julia-1.5.3"
 export JULIA_DEPOT_PATH="/dartfs-hpc/scratch/alex/.julia"
 # Probably don't have to change these:
 export JULIA_PROJECT="$JULIA_DEPOT_PATH/environments/v1.5/Project.toml"
