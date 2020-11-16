139058676b975f69b8f5131adc93900911c0df31
Brenhin Keller <cbkeller@discovery7.hpcc.dartmouth.edu>
2020-11-15 23:18:51 -0500

    Make it easier to change users in runjulia bash scripts

--- LoscarParallel/runhelloworld.bash
+++ LoscarParallel/runhelloworld.bash
@@ -1,8 +1,12 @@
 #!/bin/bash
+
 # Environment variables to tell Julia about our nonstandard install location
-export JULIA_BINDIR="/dartfs-hpc/scratch/alex/julia-1.5.3/bin"
+# Change these if you change users:
+export JULIA_DIR = "dartfs-hpc/scratch/alex/julia-1.5.3"
 export JULIA_DEPOT_PATH="/dartfs-hpc/scratch/alex/.julia"
-export JULIA_PROJECT="/dartfs-hpc/scratch/alex/.julia/environments/v1.5/Project.toml"
-export JULIA_LOAD_PATH="/dartfs-hpc/scratch/alex/.julia/environments/v1.5/Project.toml:/dartfs-hpc/scratch/alex/julia-1.5.3/share/julia/stdlib/v1.5"
+# Probably don't have to change these:
+export JULIA_PROJECT="$JULIA_DEPOT_PATH/environments/v1.5/Project.toml"
+export JULIA_LOAD_PATH="$JULIA_DEPOT_PATH/environments/v1.5/Project.toml:$JULIA_DIR/share/julia/stdlib/v1.5"
+
 # Run Julia!
-$JULIA_BINDIR/julia --project=$JULIA_PROJECT ./helloworld.jl
+$JULIA_DIR/bin/julia --project=$JULIA_PROJECT ./helloworld.jl
