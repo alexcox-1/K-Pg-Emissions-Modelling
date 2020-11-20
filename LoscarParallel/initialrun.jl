b541cf3736a741aadc91da2f5438198b6caed948
= <=>
2020-11-20 08:45:03 -0500

    update

--- LoscarParallel/initialrun.jl
+++ LoscarParallel/initialrun.jl
@@ -1,2 +1,13 @@
 # see what's up
-test = importdataset("all_temps.csv",',');
+using StatGeochem
+temps1e4 = importdataset("1e5_temps.csv",',');
+temps1e4 = temps1e4["temps"];
+temps1e4 = reshape(temps1e4,300,1600);
+co21e4 = importdataset("1e5_co2.csv",',');
+co21e4 = co21e4["co2"];
+co21e4 = reshape(co21e4,300,1600);
+so21e4 = importdataset("1e5_so2.csv",',');
+so21e4 = so21e4["so2"];
+so21e4 = reshape(so21e4,300,1600);
+ll1e4 = importdataset("1e5_ll.csv",',');
+ll1e4 = ll1e4["ll"];
--- LoscarParallel/loscarmcmcp.jl
+++ LoscarParallel/loscarmcmcp.jl
@@ -34,16 +34,8 @@ let
     # co2 and so2 emissions.
     # characteristic Pg/y will be 0.01 - 0.1
     # change these to log
-    co2vals = zeros(300);
-    co2vals[1:75] .= 0.045;
-    co2vals[76:125] .= 0.005;
-    co2vals[106:150] .= 0.065;
-    co2vals[151:200] .= 0.0;
-    co2vals[201:250] .= 0.075;
-    co2vals[251:300] .= 0.035;
+    co2vals = zeros(300) .+ 0.05;
     svals = zeros(300) .+ 0.01;
-    svals[75:125] .= 0.02;
-    svals[251:300] .= 0.02; 
     logco2vals = log.(co2vals);
     logsvals = log.(svals);
 
