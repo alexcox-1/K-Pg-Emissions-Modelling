4dcb6c29368c2da7dd2ae33a0a46e2f69bb518f2
= <=>
2020-11-20 13:56:26 -0500

    update

--- Loscar-2.0.4.3/plotloscar.jl
+++ Loscar-2.0.4.3/plotloscar.jl
@@ -2,7 +2,5 @@ using DelimitedFiles, Plots, Interpolations
 co2doubling = 3;
 
 time_vals = readdlm("tmv.dat", '\t', Float32, '\n')  
-pco2 = readdlm("pco2a.dat", '\t', Float32, '\n')
-temp = (pco2./600) .- 1;
-temp = 3 .*temp;
-plot(time_vals,pco2,lw=2)
+d13c = readdlm("d13c.dat")
+
--- LoscarParallel/initialrun.jl
+++ LoscarParallel/initialrun.jl
@@ -1,13 +1,13 @@
 # see what's up
 using StatGeochem
-temps1e4 = importdataset("1e5_temps.csv",',');
-temps1e4 = temps1e4["temps"];
-temps1e4 = reshape(temps1e4,300,1600);
-co21e4 = importdataset("1e5_co2.csv",',');
-co21e4 = co21e4["co2"];
-co21e4 = reshape(co21e4,300,1600);
-so21e4 = importdataset("1e5_so2.csv",',');
-so21e4 = so21e4["so2"];
-so21e4 = reshape(so21e4,300,1600);
-ll1e4 = importdataset("1e5_ll.csv",',');
-ll1e4 = ll1e4["ll"];
+temps2e4 = importdataset("2e4_temps.csv",',');
+temps2e4 = temps2e4["temps"];
+temps2e4 = reshape(temps2e4,300,25600);
+co22e4 = importdataset("2e4_co2.csv",',');
+co22e4 = co22e4["co2"];
+co22e4 = reshape(co22e4,300,25600);
+so22e4 = importdataset("2e4_so2.csv",',');
+so22e4 = so22e4["so2"];
+so22e4 = reshape(so22e4,300,25600);
+ll2e4 = importdataset("2e4_ll.csv",',');
+ll2e4 = ll2e4["ll"];
